e19861c3258e02bd2160f428277eb7b54aada7b4
scardona55 <sergio.1701910394@ucaldas.edu.co>
2023-11-07 23:07:04 -0500

    actualizacion ventana consultar presupuesto

--- ants/screens/Presupuesto/consultaPresupuesto.js
+++ ants/screens/Presupuesto/consultaPresupuesto.js
@@ -4,13 +4,11 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css'; // Importa los estilos CSS
 
 function ConsultaPresupuestoMensualScreen() {
-    const [fecha, setFecha] = useState(null); // Estado para la fecha
-    const [presupuesto, setPresupuesto] = useState(0); // Estado para el presupuesto, inicialmente 0
-    const [consultarPresupuesto, setConsultarPresupuesto] = useState(null); // Estado para los datos consultados
+    const [fecha, setFecha] = useState(null);
+    const [presupuesto, setPresupuesto] = useState(0);
+    const [consultarPresupuesto, setConsultarPresupuesto] = useState(null);
 
     const handleConsultarPresupuesto = () => {
-        // Aquí implementa la lógica para consultar el presupuesto según la fecha seleccionada
-        // Supongamos que presupuestoConsultado es el resultado de la consulta
         const presupuestoConsultado = obtenerPresupuestoPorFecha(fecha);
         setConsultarPresupuesto(presupuestoConsultado);
     };
@@ -18,9 +16,9 @@ function ConsultaPresupuestoMensualScreen() {
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Consulta de Presupuesto Mensual</Text>
-            <Text style={styles.text}>Fecha</Text>
+            <Text style={styles.label}>Fecha</Text>
             <DatePicker
-                style={styles.datePicker} // Establece un estilo específico para DatePicker
+                style={styles.datePicker}
                 selected={fecha}
                 onChange={(date) => setFecha(date)}
                 dateFormat="yyyy-MM"
@@ -29,6 +27,17 @@ function ConsultaPresupuestoMensualScreen() {
             <Button title="Consultar" color="#63a1ff" onPress={handleConsultarPresupuesto} />
             {consultarPresupuesto && (
                 <View style={styles.resultContainer}>
+                    <Text style={styles.resultText}>Resumen de Presupuesto</Text>
+                    <View style={styles.tableRow}>
+                        <Text style={styles.tableCell}>Descripción</Text>
+                        <Text style={styles.tableCell}>Cantidad</Text>
+                    </View>
+                    {consultarPresupuesto.detalles.map((detalle, index) => (
+                        <View style={styles.tableRow} key={index}>
+                            <Text style={styles.tableCell}>{detalle.descripcion}</Text>
+                            <Text style={styles.tableCell}>{detalle.cantidad}</Text>
+                        </View>
+                    ))}
                     <Text style={styles.resultText}>Presupuesto Ingresado: {consultarPresupuesto.presupuestoIngresado}</Text>
                     <Text style={styles.resultText}>Presupuesto Actual: {consultarPresupuesto.presupuestoActual}</Text>
                 </View>
@@ -39,11 +48,19 @@ function ConsultaPresupuestoMensualScreen() {
 
 const obtenerPresupuestoPorFecha = (fecha) => {
     // Implementa la lógica para obtener el presupuesto por la fecha
-    // Puedes consultar tu fuente de datos o realizar cálculos necesarios aquí
-    const presupuestoIngresado = 1000; // Ejemplo: presupuesto ingresado
-    const gastosRegistrados = 200; // Ejemplo: gastos registrados
-    const presupuestoActual = presupuestoIngresado - gastosRegistrados; // Ejemplo: presupuesto actual
-    return { presupuestoIngresado, presupuestoActual };
+    // Supongamos que presupuestoConsultado es el resultado de la consulta
+    const presupuestoIngresado = 1000;
+    const gastosRegistrados = 200;
+    const presupuestoActual = presupuestoIngresado - gastosRegistrados;
+
+    // Supongamos que detalles es un array con descripciones y cantidades
+    const detalles = [
+        { descripcion: 'Detalle 1', cantidad: 100 },
+        { descripcion: 'Detalle 2', cantidad: 150 },
+        { descripcion: 'Detalle 3', cantidad: 50 },
+    ];
+
+    return { presupuestoIngresado, presupuestoActual, detalles };
 };
 
 const styles = StyleSheet.create({
@@ -66,17 +83,17 @@ const styles = StyleSheet.create({
         marginBottom: 10,
     },
     datePicker: {
-        width: '80%', // Ajusta el ancho para que coincida con los TextInput
+        width: '80%',
         height: 40,
         borderColor: 'gray',
         borderWidth: 1,
         borderRadius: 20,
         padding: 10,
-        marginBottom: 10, // Espacio entre el DatePicker y el botón Consultar
+        marginBottom: 10,
         backgroundColor: '#fff',
     },
     resultContainer: {
-        marginTop: 20, // Espacio entre el botón Consultar y los resultados
+        marginTop: 20,
         padding: 10,
         borderColor: 'gray',
         borderWidth: 1,
@@ -87,6 +104,16 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: '#0F0E0E',
         marginBottom: 10,
+    },
+    tableRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 5,
+    },
+    tableCell: {
+        flex: 1,
+        fontSize: 16,
+        color: '#0F0E0E',
     },
 });
 
