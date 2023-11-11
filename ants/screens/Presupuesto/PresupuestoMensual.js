e737f8d70443ddae460aae6499833ffa4df6b7c4
scardona55 <sergio.1701910394@ucaldas.edu.co>
2023-11-10 20:39:16 -0500

    modificaciones de la parte visual

--- ants/screens/Presupuesto/PresupuestoMensual.js
+++ ants/screens/Presupuesto/PresupuestoMensual.js
@@ -6,7 +6,6 @@ import 'react-datepicker/dist/react-datepicker.css'; // Importa los estilos CSS
 function PresupuestoMensualScreen() {
     const [monto, setMonto] = useState('');
     const [fecha, setFecha] = useState(null); // Estado para la fecha
-    const [descripcion, setDescripcion] = useState('');
 
     const handleAceptarIngreso = () => {
         // Lógica de ingreso de presupuesto mensual
@@ -25,18 +24,11 @@ function PresupuestoMensualScreen() {
             />
             <Text style={styles.text}>Fecha</Text>
             <DatePicker
-                style={styles.datePicker} // Establece un estilo específico para DatePicker
+                style={[styles.datePicker, { marginBottom: 20 }]} // Agrega marginBottom para separar del botón
                 selected={fecha}
                 onChange={(date) => setFecha(date)}
                 dateFormat="yyyy-MM"
                 showMonthYearPicker
-            />
-            <Text style={styles.text}>Descripción</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Descripcion"
-                onChangeText={(text) => setDescripcion(text)}
-                value={descripcion}
             />
             <Button title="Aceptar Ingreso" color="#63a1ff" onPress={handleAceptarIngreso} />
         </View>
@@ -74,7 +66,6 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 20,
         padding: 10,
-        marginBottom: 20,
         backgroundColor: '#fff',
     },
 });
